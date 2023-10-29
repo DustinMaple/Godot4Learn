21a9ded1492e653c25a3cd7df43e347eada4af45
DustinMaple <gjf66924011@gmail.com>
2023-10-30 02:19:37 +0800

    添加重新开始功能和音乐

--- demo3d/main.gd
+++ demo3d/main.gd
@@ -5,6 +5,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$SpawnTimer.start()
+	$UserInterface/Retry.hide()
 #	var mob = mob_scene.instantiate()
 #	mob.initialize(Vector3(0.004169, 0.009606, -12.06451), $Player.position)
 #	add_child(mob)
@@ -30,4 +31,9 @@ func _on_spawn_timer_timeout():
 
 func _on_player_dead():
 	$SpawnTimer.stop()
+	$UserInterface/Retry.show()
 	pass # Replace with function body.
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
